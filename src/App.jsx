fac2211b6d54b1ff1d5a9a1ba21ba42791c05a65
paulawon <166189769+paulawon@users.noreply.github.com>
2024-05-14 01:03:51 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Favorites from "./views/Favorites";
 import Home from "./views/Home";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 const App = () => {
   return (
@@ -21,4 +22,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
